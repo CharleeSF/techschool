17d7123d8afe95fe482792ff16c67e049670a56c
CharleeF <charleetje@gmail.com>
2017-12-25 13:17:54 +0100

    Added first components and minor styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from 'app/main/main.component';
+import { PlanningComponent } from './planning/planning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PlanningComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: MainComponent },
+      { path: 'planning', component: PlanningComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
